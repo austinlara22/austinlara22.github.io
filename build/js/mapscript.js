9679b0d999db5542ce7a109ccf3d992928b7cf4a
austinlara22 <austin.lara22@trinityschoolnyc.org>
2021-11-28 17:21:07 -0500

    Initial commit

--- build/js/mapscript.js
+++ build/js/mapscript.js
@@ -1,23 +1,24 @@
-d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv', function(err, rows){
+d3.json('Commuting.json', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
       }
 
       var data = [{
           type: 'choropleth',
-          locationmode: 'USA-states',
-          locations: unpack(rows, 'code'),
-          z: unpack(rows, 'total exports'),
-          text: unpack(rows, 'state'),
+          locationmode: 'geojson-id',
+          locations: unpack(rows, 'fips'),
+          geojson: "../data/Community Districts.geojson",
+          z: unpack(rows, 'data'),
+          text: unpack(rows, 'location'),
           zmin: 0,
-          zmax: 17000,
+          zmax: 0.4,
           colorscale: [
               [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
               [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
               [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
           ],
           colorbar: {
-              title: 'Millions USD',
+              title: 'Percent',
               thickness: 0.2
           },
           marker: {
@@ -30,11 +31,9 @@ d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_expo
 
 
       var layout = {
-          title: '2011 US Agriculture Exports by State',
+          title: 'Perecent of Population with commutes to work over 1 hour',
           geo:{
-              scope: 'usa',
-              showlakes: true,
-              lakecolor: 'rgb(255,255,255)'
+              fitbounds: "geojson",
           }
       };
 
