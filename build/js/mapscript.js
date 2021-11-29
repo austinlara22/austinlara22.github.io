08fae271672195c4868da5e43ab5acb7935905b8
austinlara22 <austin.lara22@trinityschoolnyc.org>
2021-11-29 12:21:46 -0500

    Initial commit

--- build/js/mapscript.js
+++ build/js/mapscript.js
@@ -1,3 +1,7 @@
+let buttons = document.getElementsByClassName("dataset");
+
+
+
 d3.json('https://austinlara22.github.io/data/Commuting.json', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
