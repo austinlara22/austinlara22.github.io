91acc5f2393babacc9b27fc7583124d81c55ea61
austinlara22 <austin.lara22@trinityschoolnyc.org>
2021-11-28 19:39:34 -0500

    Update mapscript.js

--- build/js/mapscript.js
+++ build/js/mapscript.js
@@ -7,7 +7,7 @@ d3.json('https://austinlara22.github.io/data/Commuting.json', function(err, rows
           type: 'choropleth',
           locationmode: 'geojson-id',
           locations: unpack(rows, "fips"),
-          featureidkey: boro_cd,
+          featureidkey: "boro_cd",
           geojson: "../data/Community Districts.geojson",
           z: unpack(rows, 'data'),
           text: unpack(rows, 'location'),
