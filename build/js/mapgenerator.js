6e201d58db77d0ce4d5454f137db7249ebf53981
austinlara22 <austin.lara22@trinityschoolnyc.org>
2021-12-01 09:24:43 -0500

    Initial commit

--- build/js/mapgenerator.js
+++ build/js/mapgenerator.js
@@ -1,44 +1,13 @@
-d3.json('../data/commuting.json', function(err, rows){
+d3.json('https://austinlara22.github.io/data/Commuting.json', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
       }
 
-       var data = [{
-            type: 'choropleth',
-            locations: unpack(rows, 'CODE'),
-            z: unpack(rows, 'GDP (BILLIONS)'),
-            text: unpack(rows, 'COUNTRY'),
-            colorscale: [
-                [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-                [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-                [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
-            autocolorscale: false,
-            reversescale: true,
-            marker: {
-                line: {
-                    color: 'rgb(180,180,180)',
-                    width: 0.5
-                }
-            },
-            tick0: 0,
-            zmin: 0,
-            dtick: 1000,
-            colorbar: {
-                autotic: false,
-                tickprefix: '$',
-                title: 'GDP<br>Billions US$'
-            }
-      }];
 
-      var layout = {
-          title: '2014 Global GDP<br>Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html"> CIA World Factbook</a>',
-          geo:{
-              showframe: false,
-              showcoastlines: false,
-              projection:{
-                  type: 'mercator'
-              }
-          }
-      };
-      TESTER = document.getElementById('tester');
-Plotly.newPlot( TESTER , data, layout, {showLink: false})
+var trace = {
+    x: unpack(rows,"data"),
+    type: 'histogram',
+  };
+var data = [trace];
+Plotly.newPlot('myDiv_01', data);
+});
